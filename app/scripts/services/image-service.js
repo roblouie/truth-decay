96978a571110cdab574d6147be43b0d768ce3110
Rob Louie <rob.j.louie@gmail.com>
2015-09-11 01:03:20 -0500

    Finish gallery

--- app/scripts/services/image-service.js
+++ app/scripts/services/image-service.js
@@ -8,25 +8,62 @@
  * Service in the truthDecayApp.
  */
 angular.module('truthDecayApp')
-  .service('imageService', function () {
-    return {
-        getShotLinks: getShotLinks,
-        getThumbnailLinks: getThumbnailLinks
-    };
+    .service('imageService', function () {
+        return {
+            getFullFrameThumbnails: getFullFrameThumbnails,
+            getFullFrameShots: getFullFrameShots,
+            getJockThumbnails: getJockThumbnails,
+            getJockShots: getJockShots,
+            getTeethThumbnails: getTeethThumbnails,
+            getTeethShots: getTeethShots,
+            getAllThumbnails: getAllThumbnails,
+            getAllShots: getAllShots,
+            getRandomImage: getRandomImage
+        };
 
-    function getShotLinks() {
-        return getLinks();
-    }
+        function getFullFrameThumbnails()   { return getFullFrameLinks(true) }
+        function getFullFrameShots()        { return getFullFrameLinks(false) }
+        function getJockThumbnails()        { return getJockLinks(true) }
+        function getJockShots()             { return getJockLinks(false) }
+        function getTeethThumbnails()       { return getTeethLinks(true) }
+        function getTeethShots()            { return getTeethLinks(false) }
+        function getAllThumbnails()         { return getAllLinks(true) }
+        function getAllShots()              { return getAllLinks(false) }
 
-    function getThumbnailLinks() {
-        return getLinks(true);
-    }
-
-    function getLinks(isThumbnails) {
-        var urlList = [];
-        for (var i = 1; i <= 25; i++) {
-            urlList.push("/images/" + (isThumbnails ? "thumbnails/" : "shots/") + i + ".JPG");
+        function getFullFrameLinks(isThumbnails) {
+            var urlList = [];
+            for (var i = 22; i <=25; i++) {
+                urlList.push("/images/" + (isThumbnails ? "thumbnails/" : "shots/") + i + ".JPG");
+            }
+            return urlList;
         }
-        return urlList;
-    }
-  });
+
+        function getJockLinks(isThumbnails) {
+            var urlList = [];
+            for (var i = 1; i <= 7; i++) {
+                urlList.push("/images/" + (isThumbnails ? "thumbnails/" : "shots/") + i + ".JPG");
+            }
+            urlList.push("/images/" + (isThumbnails ? "thumbnails/" : "shots/") + 15 + ".JPG");
+            return urlList;
+        }
+
+        function getTeethLinks(isThumbnails) {
+            var urlList = [];
+            for (var i = 8; i <= 14; i++) {
+                urlList.push("/images/" + (isThumbnails ? "thumbnails/" : "shots/") + i + ".JPG");
+            }
+            for (var i = 16; i <= 21; i++) {
+                urlList.push("/images/" + (isThumbnails ? "thumbnails/" : "shots/") + i + ".JPG");
+            }
+            return urlList;
+        }
+
+        function getAllLinks(isThumbnails) {
+            return getFullFrameLinks(isThumbnails).concat(getJockLinks(isThumbnails).concat(getTeethLinks(isThumbnails)));
+        }
+
+        function getRandomImage() {
+            var imageNumberToShow = Math.floor(Math.random() * (25 - 1 + 1)) + 1;
+            return "/images/shots/" + imageNumberToShow + ".JPG";
+        }
+    });
